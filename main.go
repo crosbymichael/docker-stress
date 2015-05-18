cc81a91ad86ff18ff635a58d5cf54c5d338572de
Michael Crosby <crosbymichael@gmail.com>
2015-05-18 16:00:39 -0700

    Updates to better cli

--- main.go
+++ main.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
-	"log"
 	"os"
 	"os/exec"
 	"sync"
 	"syscall"
 	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
 )
 
 var (
-	concurrent int
-	duration   time.Duration
-	killTime   time.Duration
-	binary     string
-
-	counter int
+	counter   int
+	failCount int
 )
 
 type Image struct {
@@ -27,54 +24,58 @@ type Image struct {
 	Kill    bool     `json:"kill"`
 }
 
-func init() {
-	flag.IntVar(&concurrent, "concurrent", 1, "number of concurrent workers to run")
-
-	flag.DurationVar(&duration, "duration", 10*time.Minute, "duration to run the tests")
-	flag.DurationVar(&killTime, "kill", 10*time.Second, "time to kill a container after")
-
-	flag.StringVar(&binary, "binary", "docker", "path to docker binary")
-
-	flag.Parse()
+func newWorker(binary string, kill time.Duration, group *sync.WaitGroup) *worker {
+	return &worker{
+		binary:   binary,
+		killTime: kill,
+		wg:       group,
+	}
 }
 
-func run(i *Image) {
-	counter++
+type worker struct {
+	binary   string
+	killTime time.Duration
+	wg       *sync.WaitGroup
+}
 
+func (w *worker) start(c chan *Image) {
+	defer w.wg.Done()
+	for i := range c {
+		w.run(i)
+	}
+}
+
+func (w *worker) run(i *Image) {
+	counter++
 	p := "-P=false"
 	if i.Publish {
 		p = "-P=true"
 	}
-
-	cmd := exec.Command(binary, append([]string{"run", p, "--rm", i.Name}, i.Args...)...)
+	cmd := exec.Command(w.binary, append([]string{"run", p, "--rm", i.Name}, i.Args...)...)
 	if i.Kill {
 		go func() {
-			<-time.After(killTime)
-			cmd.Process.Signal(syscall.SIGTERM)
+			<-time.After(w.killTime)
+			if err := cmd.Process.Signal(syscall.SIGTERM); err != nil {
+				logrus.Error(err)
+			}
+			if err := cmd.Wait(); err != nil {
+				logrus.Error(err)
+			}
 		}()
 	}
-
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Println(string(out))
+		failCount++
+		logrus.WithField("error", err).Errorf("%s", out)
 	}
 }
 
-func worker(group *sync.WaitGroup, c chan *Image) {
-	defer group.Done()
-
-	for i := range c {
-		run(i)
-	}
-}
-
-func loadImages() ([]*Image, error) {
-	f, err := os.Open("stress.json")
+func loadImages(path string) ([]*Image, error) {
+	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-
 	var images []*Image
 	if err := json.NewDecoder(f).Decode(&images); err != nil {
 		return nil, err
@@ -82,43 +83,51 @@ func loadImages() ([]*Image, error) {
 	return images, nil
 }
 
-func process(images []*Image, c chan *Image) {
-	after := time.After(duration)
+func process(images []*Image, c chan *Image, max int) {
 	for {
+		if counter > max {
+			close(c)
+			return
+		}
 		for _, i := range images {
-			select {
-			case <-after:
-				close(c)
-				return
-			default:
-				c <- i
-			}
+			c <- i
 		}
 	}
 }
 
 func main() {
-	var (
-		c     = make(chan *Image, concurrent)
-		group = &sync.WaitGroup{}
-		start = time.Now()
-	)
-
-	images, err := loadImages()
-	if err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+	app.Name = "stress"
+	app.Usage = "stress test your docker daemon"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{Name: "binary,b", Value: "docker", Usage: "path to the docker binary to test"},
+		cli.StringFlag{Name: "config", Value: "stress.json", Usage: "path to the stress test configuration"},
+		cli.IntFlag{Name: "concurrent,c", Value: 1, Usage: "number of concurrent workers to run"},
+		cli.IntFlag{Name: "containers", Value: 1000, Usage: "number of containers to run"},
+		cli.DurationFlag{Name: "kill,k", Value: 10 * time.Second, Usage: "time to kill a container after an execution"},
 	}
-
-	for i := 0; i < concurrent; i++ {
-		group.Add(1)
-		go worker(group, c)
+	app.Action = func(context *cli.Context) {
+		var (
+			c     = make(chan *Image, context.GlobalInt("concurrent"))
+			group = &sync.WaitGroup{}
+			start = time.Now()
+		)
+		images, err := loadImages(context.GlobalString("config"))
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		for i := 0; i < context.GlobalInt("concurrent"); i++ {
+			group.Add(1)
+			w := newWorker(context.GlobalString("binary"), context.GlobalDuration("kill"), group)
+			go w.start(c)
+		}
+		go process(images, c, context.GlobalInt("containers"))
+		group.Wait()
+		secounds := time.Now().Sub(start).Seconds()
+		logrus.Infof("ran %d containers in %f seconds (%f per sec.)\n", counter, secounds, float64(counter)/secounds)
+		logrus.Infof("failures %d\n", failCount)
 	}
-
-	go process(images, c)
-
-	group.Wait()
-
-	secounds := time.Now().Sub(start).Seconds()
-
-	log.Printf("ran %d containers in %f seconds (%f per sec.)\n", counter, secounds, float64(counter)/secounds)
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
 }
